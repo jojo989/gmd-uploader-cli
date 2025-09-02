d3ebe32051f04b40359c2b038577dda09fe518d5
jojo989 <70300454+jojo989@users.noreply.github.com>
2025-09-02 16:13:02 -0400

    fixed some default args

--- main.py
+++ main.py
@@ -9,15 +9,13 @@ def main():
     parser.add_argument('-d', '--description', default="", help="Level description")
     parser.add_argument('--id', "-id", type=int, default=0, help="Update existing level ID (default: 0 for new level)")
     parser.add_argument('--gameversion' , "-v", type=int, default=22, choices=range(0, 22), metavar="[0-22]", help="Game version number (21 or 22)")
-    parser.add_argument('--levelversion', type=int, default=127, choices=range(1, 127), metavar="[1-127]", 
+    parser.add_argument('--levelversion', type=int, default=0, choices=range(1, 127), metavar="[1-127]", 
                         help="Level version number (max 127)")
     parser.add_argument('--gmd', "-gmd", required=True, help="Path to .gmd file containing level data")
     parser.add_argument('-usr', '--username', dest='username', required=True, help="Username")
     parser.add_argument('-pwd', '--password', dest='password', required=True, help="Password")
     parser.add_argument('-m', '--mode', type=int, default=0, choices=[0, 1, 2], 
                         help="Visibility mode: 0 (public), 1 (unlisted), 2 (friends only)")
-    parser.add_argument('--songid', help="Newgrounds song ID", default=get_gmd_value_by_key(gmd_content, 'k35'))
-
     args = parser.parse_args()
     if not args.gmd.lower().endswith('.gmd'):
         print(f"Error: '{args.gmd}' is not a .gmd file. Please provide a valid .gmd file.")
@@ -31,6 +29,14 @@ def main():
     except Exception as e:
         print(f"Error reading .gmd file: {e}")
         return
+
+    if not hasattr(args, 'songid') or args.songid is None:
+        songid_value = get_gmd_value_by_key(gmd_content, 'k35')
+        try:
+            args.songid = int(songid_value) if songid_value is not None else 0
+        except ValueError:
+            print(f"Error: songid: '{songid_value}' ")
+            return
 
     level_string = get_gmd_value_by_key(gmd_content, 'k4')
     if not level_string:
